4d1bb71c307726b28b0a73413e3b0037d3a0c287
Jens Simon <64278468+jens-simon@users.noreply.github.com>
2023-11-15 21:51:12 +0100

    die loesungen zu den uebungen 1 und 2

---
+++ jens_2023_11_15_php/02_uebung/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        username <input type="text" name="username" value="<?php echo 'Mr. X'; ?>" /><br>
+        password <input type="password" name="password" value="<?php echo '*****'; ?>" /><br>
+        <input type="submit" value="login" />
+    </form>
+</body>
+
+</html>
