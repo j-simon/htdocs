f1237aed123a394fdcaee57cd37ae42ddde13978
Jens Simon <64278468+jens-simon@users.noreply.github.com>
2023-11-21 08:27:07 +0100

    falsches Datum im Ordner von gestern

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+   //declare(strict_types=1); // erzeugt einen Fehler bei falschen Datentypen
+
+   function f( int $x,  int $y=1) : int{
+      $ergebnis = $x + $y;
+
+      return $ergebnis;
+   }
+
+   echo f(1,2); // integers - korrekter Aufruf
+
+   echo f(1.1,2.2); // floats - nicht ganz korrekt - 
+                    // erzeugt depraceted oder error meldung je nachdem ob  strict_types deklriert ist,
+                    // oder nicht   
+
+   echo f("1.1","2.2"); // strings - nicht ganz korrekt - 
+                        // erzeugt depraceted oder error meldung je nachdem ob  strict_types deklariert ist,
+                        // oder nicht   
+   
